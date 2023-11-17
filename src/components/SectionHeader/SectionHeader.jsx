c25258efce609128849a055ab83b39fb6e59336f
MDMahidul <mmbmahidul007@gmail.com>
2023-11-17 18:47:19 +0600

    added testimony and facilities sections and add fadein up animation using framer motion

--- src/components/SectionHeader/SectionHeader.jsx
+++ src/components/SectionHeader/SectionHeader.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import FadeInAnimation from '../FadeInAnimation/FadeInAnimation';
 
 const SectionHeader = ({heading}) => {
     return (
-      <div className="font-second_font text-center md:w-4/12 mx-auto py-10 ">
-        <h3 className="text-4xl dark:text-white text-gray-900 py-4 ">{heading}</h3>
-      </div>
+      <FadeInAnimation custom={1}>
+        <div className="font-second_font text-center md:w-4/12 mx-auto py-10 ">
+          <h3 className="text-4xl dark:text-white text-gray-900 py-4 ">
+            {heading}
+          </h3>
+        </div>
+      </FadeInAnimation>
     );
 };
 
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner/Banner';
 import SportsProgrammes from '../SportsProgrammes/SportsProgrammes';
+import Facilities from '../Facilities/Facilities';
+import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
     return (
       <div>
         <Banner/>
         <SportsProgrammes/>
+        <Facilities/>
+        <Testimonials/>
       </div>
     );
 };
